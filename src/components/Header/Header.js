d36682e00ae54b9f32a42ca9c8d294acc7f66b88
Richard Chiu <richard.chiu87@gmail.com>
2023-03-02 14:12:21 -0500

    header component done, basic styling for mobile tablet and desktop done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import "./Header.scss";
+import logo from "../../assets/images/BrainFlix-logo.svg";
+import mohan from "../../assets/images/Mohan-muruge.jpg";
+import uploadIcon from "../../assets/images/upload.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <img className="header__logo" src={logo} alt="Branflix Logo"></img>
+        <form className="header__form">
+          <div className="header__form-container">
+            <input
+              className="form__search"
+              type="search"
+              name="search"
+              placeholder="Search"
+            ></input>
+            <img
+              src={mohan}
+              className="defaultAvatar"
+              alt="Default avatar"
+            ></img>
+            <button className="header__button">
+              <img
+                className="header__button-icon"
+                src={uploadIcon}
+                alt="upload icon"
+              />
+              <p className="header__button-text">UPLOAD</p>
+            </button>
+          </div>
+        </form>
+      </header>
+    </>
+  );
+};
+
+export default Header;
