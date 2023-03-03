34bb432b1692dbb17a4c3672d2fbfa48a64221aa
Richard Chiu <richard.chiu87@gmail.com>
2023-03-02 21:41:23 -0500

    mobile styling for videoListContent done

--- src/components/VideoContentList/VideoContentList.js
+++ src/components/VideoContentList/VideoContentList.js
@@ -3,14 +3,18 @@ import "./VideoContentList.scss";
 const VideoContentList = (props) => {
   return (
     <>
-      <img
-        src={props.videoContentImage}
-        alt="bike tricks thumbnail"
-        className="videoListContent__image"
-      ></img>
-      <div className="videoListContent-textContainer">
-        <h4 className="videoListContent__title">{props.videoContentTitle}</h4>
-        <p className="videoListContent__channel">{props.videoContentChannel}</p>
+      <div className="videoListContent-mainContainer">
+        <img
+          src={props.videoContentImage}
+          alt="bike tricks thumbnail"
+          className="videoListContent__image"
+        ></img>
+        <div className="videoListContent-textContainer">
+          <h4 className="videoListContent__title">{props.videoContentTitle}</h4>
+          <p className="videoListContent__channel">
+            {props.videoContentChannel}
+          </p>
+        </div>
       </div>
     </>
   );
