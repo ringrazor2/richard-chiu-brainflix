f04803a8ba5e8cf7e26215f6e028470f58a29e47
Richard Chiu <richard.chiu87@gmail.com>
2023-03-10 20:42:44 -0500

    error page Link tag done

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,13 +1,14 @@
 import "./ErrorPage.scss";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <div className="errorPage">
       <h1 className="errorPage__title">404</h1>
       <p className="errorPage__message">Oops! Something is wrong.</p>
-      <a className="errorPage__anchor" href="#">
+      <Link to="/" className="errorPage__anchor">
         <i className="icon-home"></i> Go back in initial page, is better.
-      </a>
+      </Link>
     </div>
   );
 };
