0ae4c299b0569bd434346d51b1835e4d4d9060b9
Richard Chiu <richard.chiu87@gmail.com>
2023-03-10 20:36:32 -0500

    styling blue done

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -3,8 +3,11 @@ import "./ErrorPage.scss";
 const ErrorPage = () => {
   return (
     <div className="errorPage">
-      <h1 className="errorPage__title">Error: page does not exist</h1>
-      <image className="errorPage__image" src=""></image>
+      <h1 className="errorPage__title">404</h1>
+      <p className="errorPage__message">Oops! Something is wrong.</p>
+      <a className="errorPage__anchor" href="#">
+        <i className="icon-home"></i> Go back in initial page, is better.
+      </a>
     </div>
   );
 };
